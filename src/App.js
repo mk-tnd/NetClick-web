c0f4cdc311d2f033df834d62fc2b4ed5e2aeec4a
Palm <xelticross@gmail.com>
2021-05-17 20:44:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Auth from "./pages/Auth";
+// import { useContext } from "react";
+// import { AuthContext } from "./contexts/AuthContextProvider";
+
+function App() {
+  // const { isAuth } = useContext(AuthContext);
+  const isAuth = true;
+  const privateRoutes = [
+    // {
+    //   path: "/",
+    //   component: Home,
+    // },
+  ];
+
+  const publicRoutes = [
+    // {
+    //   path: "/",
+    //   component: Auth,
+    // },
+  ];
+  return (
+    <BrowserRouter>
+      <Switch>
+        {isAuth &&
+          privateRoutes.map((el, index) => (
+            <Route key={index} exact path={el.path} component={el.component} />
+          ))}
+        {!isAuth &&
+          publicRoutes.map((el, index) => (
+            <Route key={index} exact path={el.path} component={el.component} />
+          ))}
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
