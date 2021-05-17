875668b3cf5293a3e4d43a26a6da7f3328d5938f
mk-tnd <mickey_saejia@hotmail.com>
2021-05-17 19:09:07 +0700

    fix

--- src/utils/customize.js
+++ src/utils/customize.js
@@ -1,11 +1,11 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import black from "@material-ui/core/colors/black";
+import grey from "@material-ui/core/colors/grey";
 import red from "@material-ui/core/colors/red";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: black[500],
+      main: grey[900],
     },
     secondary: {
       main: red[500],
